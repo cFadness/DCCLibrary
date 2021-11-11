7d4329c7be498ae6ae2dbb1bf20747c093ff2306
cFadness <codie1989@gmail.com>
2021-11-11 08:11:39 -0800

    Complete Navigation Funcionality for going to next and previous book.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,8 +11,30 @@ class App extends Component {
             {title: "The First and Last Freedom", author: "Jiddu Krishnamurit"}
         ];
         this.state = {
-            bookNumber: 1
+            bookNumber: 0
         };
+    }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0){
+            tempBookNumber = this.books.length -1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
 
     render(){
@@ -22,6 +44,7 @@ class App extends Component {
                 <div className="row">
                     <div className="col-md-4">
                         {/*Button here to move to the previous book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
                     <div className="col-md-4">
                         {/*Display book with cover here*/}
@@ -30,9 +53,9 @@ class App extends Component {
                     </div>
                     <div className="col-md-4">
                         {/*Button here to move to the previous book viewed*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
-
             </div>
         )
     }
